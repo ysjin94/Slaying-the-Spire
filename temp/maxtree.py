a07f78f0edbfe3de70898cfcaf3845f814ae8857
john <john.durasko08@myhunter.cuny.edu>
2020-07-07 17:31:30 -0400

    added functions to return path to best move

--- temp/maxtree.py
+++ temp/maxtree.py
@@ -1,91 +1,7 @@
 import random
 from anytree import Node, RenderTree, LevelOrderGroupIter
 
-class SimGame:
-    self.in_combat = False
-    self.player = None
-    self.monsters = []
-    self.draw_pile = []
-    self.discard_pile = []
-    self.exhaust_pile = []
-    self.hand = []
-    self.limbo = []
-    self.card_in_play = None
-    self.turn = 0
-    self.cards_discarded_this_turn = 0
-    #--------- Additional
-    self.energy = 3
-    #--------- Card switches needed for combat
-    self.Barricade = False
-    self.Battle_Trace = False
-    self.Blood_For_Blood = False
-    self.Combust = False
-    self.Corruption = False
-    self.Dark_Embrace = False
-    self.Demon_Form = False
-    self.Double_Tap = False
-    self.Evolve = False
-    self.Feel_No_Pain = False
-    self.Fire_Breathing = False
-    self.Flame_Barrier = False
-    self.Flex = False
-    self.Juggernaut = False
-    self.Metallicize = False
-    self.Rage = False
-    self.Rupture = False
 
-
-
-
-def getstate:
-    n = SimGame()
-    n.player = Game.player
-    n.monsters = Game.monsters
-    n.draw_pile = Game.draw_pile
-    n.discard_pile = Game.discard_pile
-    n.exhaust_pile = Game.exhaust_pile
-    n.hand = Game.hand
-    n.limbo = Game.limbo
-    n.card_in_play = Game.card_in_play
-    n.turn = Game.turn
-    n.cards_discarded_this_turn = Game.cards_discarded_this_turn
-
-
-#---------------
-
-# Effect at end of turn
-def end_of_turn(gamestate):
-    newstate = gamestate
-    #deal poison damage
-    #ethereal check, if card is ethereal, exhaust it
-    return newstate
-
-def start_of_turn(gamestate):
-    newstate = gamestate
-    #reset energy/mana
-    return newstate
-#Need to Helper Function
-#Cost,
-
-#example pseudocode
-for cardname, card in Game.hand:
-    x = cards[card]
-    #check if enough energy to play card
-    #might have to add energy to game state
-    if SimGame.energy >= x[0]:
-        SimGame.energy = Game.energy - x[0]
-        #somehow play card/call card function from dict
-        #PROBLEM: how are monsters stores in gamestate?'
-        #since some cards don't need targets, the card function will loop through the targets if needed?
-        if x[1] = False:
-            x[2](SimGame)
-            SimGame = SimGame.hand.remove(cardname)
-            SimGame = SimGame.discard_pile.append(cardname)
-        else:
-            for target in SimGame.Monsters:
-                x[2](SimGame, target)
-                SimGame = SimGame.hand.remove(cardname)
-                SimGame = SimGame.discard_pile.append(cardname)
 
 #will need to evaluate the new gamestate the card function returns?
 
@@ -99,29 +15,46 @@ def eval_function(gamestate):
 
 
 #return new list of available cards after playing one
-def get_next_game_state(card, cards):
-    remaining_cards = cards.copy() #copy list of cards
-
-    #remove the card being played from list of cards
-    remaining_cards.remove(card)
-    return remaining_cards #return remaining list of cards
+def get_next_game_state(card, enough_energy, gamestate):
+        gs = SimGame()
+        x = 0
+        if card == 'health':
+           x = 1 
+        if card == 'potion':
+           x = 2 
+        if card == 'health':
+           x = 3 
+        if gamestate.name.energy >= x:
+            gs.hand = gamestate.name.hand.copy()
+            gs.hand.remove(card)
+            gs.discard_pile = gamestate.name.discard_pile.copy()
+            gs.discard_pile.append(card)
+            gs.energy = gamestate.name.energy
+            gs.energy -= x
+        else:
+            enough_energy = False
+        return gs 
 
 #builds a tree using current game
 def build_tree(gamestate):
-        for x in gamestate.name.card_list:
-            temp = Game_State() #new child game state
-            temp.card_list = gamestate.name.card_list #copy card list
-            temp.card_list = get_next_game_state(x, temp.card_list) #get next game state
-            if len(temp.card_list) == 1: #if this is a leaf assign evaluation
-                temp.grade = eval_function(1)
-            if temp.card_list: #if card list is not empty
+        for x in gamestate.name.hand:
+            temp = SimGame()
+            can_play = True
+            temp = get_next_game_state(x, can_play, gamestate)
+            if can_play and temp.hand: #if card list is not empty
                 child = Node(temp, parent=gamestate) #create child node
                 build_tree(child) #recursively build tree
+
+def eval_tree(r):
+    for children in LevelOrderGroupIter(r):
+        for node in children:
+            if not node.children:
+                node.name.grade = eval_function(1)
 
 def tree_search(r):
     if not r.children: #if node is a leaf
         return
-    max = -51
+    max = -101
     for children in LevelOrderGroupIter(r, maxlevel=2):
         for node in children:
             if node in r.children:
@@ -143,27 +76,70 @@ def return_path(r):
         for node in children:
             if node in r.children:
                 if node.name.grade == r.name.grade:
-                    move = return_move(r.name.card_list, node.name.card_list)
+                    move = return_move(r.name.hand, node.name.hand)
                     move_list.append(move)
                     move_list += return_path(node)
     return move_list 
 
-cards = ['card1', 'card2', 'card3']
 
-class Game_State:
-    card_list = []
-    grade = 0
+class SimGame:
+    in_combat = False
+    player = None
+    monsters = []
+    draw_pile = []
+    discard_pile = []
+    exhaust_pile = []
+    hand = []
+    limbo = []
+    card_in_play = None
+    turn = 0
+    cards_discarded_this_turn = 0
+    grade = 0 #evaluation grade
+    #--------- Additional
+    energy = 3
+    #--------- Card switches needed for combat
+    Barricade = False
+    Battle_Trace = False
+    Blood_For_Blood = False
+    Combust = False
+    Corruption = False
+    Dark_Embrace = False
+    Demon_Form = False
+    Double_Tap = False
+    Evolve = False
+    Feel_No_Pain = False
+    Fire_Breathing = False
+    Flame_Barrier = False
+    Flex = False
+    Juggernaut = False
+    Metallicize = False
+    Rage = False
+    Rupture = False
 
-a = Game_State()
-a.card_list = cards.copy()
+cards = ['health', 'potion', 'attack']
+
+a = SimGame()
+a.hand = cards.copy()
 root = Node(a, parent=None)
 build_tree(root)
 for pre, fill, node in RenderTree(root):
-    print("%s%s%s" % (pre, node.name.card_list, node.name.grade))
+    print("%s%s%s" % (pre, node.name.hand, node.name.grade))
+eval_tree(root)
+print()
+print()
+print()
+print()
+for pre, fill, node in RenderTree(root):
+    print("%s%s%s" % (pre, node.name.hand, node.name.grade))
 tree_search(root)
 print()
 print()
 print()
 print()
 for pre, fill, node in RenderTree(root):
-    print("%s%s%s" % (pre, node.name.card_list, node.name.grade))
+    print("%s%s%s" % (pre, node.name.hand, node.name.grade))
+print()
+print()
+print()
+print()
+print(return_path(root))
