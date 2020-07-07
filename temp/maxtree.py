34e0b8e6f700bbb86f27df7f354a7284afdbe565
john <john.durasko08@myhunter.cuny.edu>
2020-07-07 17:36:27 -0400

    added functions to return path to best move

--- temp/maxtree.py
+++ temp/maxtree.py
@@ -1,7 +1,71 @@
 import random
 from anytree import Node, RenderTree, LevelOrderGroupIter
 
+class SimGame:
+    self.in_combat = False
+    self.player = None
+    self.monsters = []
+    self.draw_pile = []
+    self.discard_pile = []
+    self.exhaust_pile = []
+    self.hand = []
+    self.limbo = []
+    self.card_in_play = None
+    self.turn = 0
+    self.cards_discarded_this_turn = 0
+    #--------- Additional
+    self.energy = 3
+    #--------- Card switches needed for combat
+    self.Barricade = False
+    self.Battle_Trace = False
+    self.Blood_For_Blood = False
+    self.Combust = False
+    self.Corruption = False
+    self.Dark_Embrace = False
+    self.Demon_Form = False
+    self.Double_Tap = False
+    self.Evolve = False
+    self.Feel_No_Pain = False
+    self.Fire_Breathing = False
+    self.Flame_Barrier = False
+    self.Flex = False
+    self.Juggernaut = False
+    self.Metallicize = False
+    self.Rage = False
+    self.Rupture = False
 
+
+
+
+def getstate:
+    n = SimGame()
+    n.player = Game.player
+    n.monsters = Game.monsters
+    n.draw_pile = Game.draw_pile
+    n.discard_pile = Game.discard_pile
+    n.exhaust_pile = Game.exhaust_pile
+    n.hand = Game.hand
+    n.limbo = Game.limbo
+    n.card_in_play = Game.card_in_play
+    n.turn = Game.turn
+    n.cards_discarded_this_turn = Game.cards_discarded_this_turn
+
+
+#---------------
+
+# Effect at end of turn
+def end_of_turn(gamestate):
+    newstate = gamestate
+    #deal poison damage
+    #ethereal check, if card is ethereal, exhaust it
+    return newstate
+
+def start_of_turn(gamestate):
+    newstate = gamestate
+    #reset energy/mana
+    return newstate
+#Need to Helper Function
+#Cost,
 
 #will need to evaluate the new gamestate the card function returns?
 
@@ -82,39 +146,6 @@ def return_path(r):
     return move_list 
 
 
-class SimGame:
-    in_combat = False
-    player = None
-    monsters = []
-    draw_pile = []
-    discard_pile = []
-    exhaust_pile = []
-    hand = []
-    limbo = []
-    card_in_play = None
-    turn = 0
-    cards_discarded_this_turn = 0
-    grade = 0 #evaluation grade
-    #--------- Additional
-    energy = 3
-    #--------- Card switches needed for combat
-    Barricade = False
-    Battle_Trace = False
-    Blood_For_Blood = False
-    Combust = False
-    Corruption = False
-    Dark_Embrace = False
-    Demon_Form = False
-    Double_Tap = False
-    Evolve = False
-    Feel_No_Pain = False
-    Fire_Breathing = False
-    Flame_Barrier = False
-    Flex = False
-    Juggernaut = False
-    Metallicize = False
-    Rage = False
-    Rupture = False
 
 cards = ['health', 'potion', 'attack']
 
