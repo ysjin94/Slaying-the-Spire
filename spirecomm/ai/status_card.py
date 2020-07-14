8550bdf13bd06550988c6f6f04b5e72b97af3960
SunJin Yoon <56009413+ysjin94@users.noreply.github.com>
2020-07-14 12:15:25 -0400

    Added functions

--- spirecomm/ai/status_card.py
+++ spirecomm/ai/status_card.py
@@ -1,1 +1,27 @@
 #This is status_card
+#Unplayed : cannot be played in your hand
+
+#Burn :Unplayable. At the end of your turn, take 2(4) damage
+def Burn(gamastate, upgrade):
+  newstate = gamestate
+  return newstate
+
+#Dazed : Unplayable. Ethereal
+def Dazed(gamastate, upgrade):
+  newstate = gamestate
+  return newstate
+
+#Wound : Unplayable
+def Wound(gamastate, upgrade):
+  newstate = gamestate
+  return newstate
+
+#Slimed : Exhuast
+def Slimed(gamastate, upgrade):
+  newstate = gamestate
+  return newstate
+
+#Void : Unplayable. When this card is drawn, lose 1 Energy. Ethereal.
+def Void(gamastate, upgrade):
+  newstate = gamestate
+  return newstate
