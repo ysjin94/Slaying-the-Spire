ee1fcf88bad561a628cb1621f078bae89e24ce2b
SunJin Yoon <56009413+ysjin94@users.noreply.github.com>
2020-07-14 12:38:02 -0400

    Update Card lists

--- spirecomm/ai/colorless_card.py
+++ spirecomm/ai/colorless_card.py
@@ -1,1 +1,50 @@
 #This is colorless_card
+
+#Bandage Up : cost 0, Heal 4(6)HP exhaust
+#Blind : cost 0, Apply 2 weak (to ALL enemies)
+#Dard shackles : cost 0, Enemy loses 9(15) Icon Strength Strength for the rest of this turn. Exhaust.
+#Deep Breath : cost 0 , Shuffle your discard pile into your draw pile. Draw 1(2) card(s).
+#Discovery : cost 1, Choose 1 of 3 random cards to add to your hand. It costs 0 this turn. Exhaust. (Don't Exhaust.)
+#Dramatic Entrance : cost 0 , Innate. Deal 6(8) damage to ALL enemies. Exhaust.
+#Enlightenment : cost 0 , Reduce the cost of cards in your hand to 1 this turn(combat)
+#Finesse : cost 0 Gain 2(4) Icon Block Block. Draw 1 card.
+#Flash of Steel : cost 0, Deal 3(6) damage. Draw 1 card.
+#Forethought : cost, Place a card(any number of cards) from your hand on the bottom of your draw pile. It (They) costs 0 until played.
+#Good instincts : cost 0, Gain 5(8) Icon Block Block.
+#impatience : cost 0, If you have no Attack cards in your hand, draw 2(3) cards.
+#Jack Of All Trades : cost 0, Add 1(2) random Colorless card(s) to your hand.
+#Madness : cost 1(0), A random card in your hand costs 0 for the rest of combat. Exhaust.
+#Mind Blast : cost 2(1), Innate. Deal damage equal to the number of cards in your draw pile.
+#Panacea : cost 0, Gain 1(2) Icon Artifact Artifact.Exhaust.
+#Panic Button : cost 0, Gain 30(40) Icon Block Block. You cannot gain Icon Block Block from cards for the next 2 turns. Exhaust.
+#Purity : cost 0, Choose and exhaust 3(5) cards in your hand. Exhaust.
+#Swift Strike : cost 0, Deal 6(9) damage.
+#Trip : cost 0, Apply 2 Icon Vulnerable Vulnerable (to ALL enemies).
+#Apotheosis : cost 2(1), Upgrade ALL of your cards for the rest of combat. Exhaust.
+#Chrysalis : cost 2, Add 3(5) random Skills into your Draw Pile. They cost 0 this combat. Exhaust.
+#Hand of Greed : cost 2 , Deal 20(25) damage. If this kills a non-minion enemy, gain 20(25) Gold.
+#magnetism : cost 2(1), At the start of each turn, add a random colorless card to your hand.
+#Master Of Strategy : cost 0, Draw 3(4) cards. Exhaust.
+#Mayhem : cost 2(1), At the start of your turn, play the top card of your draw pile.
+#Metamorphosis: cost 2, Add 3(5) random Attacks into your Draw Pile. They cost 0 this combat. Exhaust.
+#Panache : cost 0, Every time you play 5 cards in a single turn, deal 10(14) damage to ALL enemies.
+#Sadistic Nature : cost 0, Whenever you apply a Debuff to an enemy, they take 3(4) damage.
+#Secret Technique : cost 0, Choose a Skill from your draw pile and place it into your hand. Exhaust. (Don't Exhaust)
+#Secret Weapon : cost 0, Choose an Attack from your draw pile and place it into your hand. Exhaust. (Don't Exhaust)
+#The Bomb : cost 2, At the end of 3 turns, deal 40(50) damage to ALL enemies.
+#Thinking Ahead : cost 0, Draw 2 cards. Place a card from your hand on top of your draw pile. Exhaust.(Don't Exhaust)
+#Transmutation :cost X, Add X random (Upgraded) colorless cards into your hand. They cost 0 this turn. Exhaust.
+#Violence : cost 0 , Place 3(4) random Attack cards from your draw pile into your hand. Exhaust.
+#Apparition : cost 1, Gain 1 Icon Intangible Intangible. Exhaust. Ethereal. (no longer Ethereal.)(Obtained from event: Council of Ghosts).
+#Beta : cost 2(1), Shuffle an Omega into your draw pile. Exhaust. (Obtained from Alpha).
+#Bite : cost 1, Deal 7(8) damage. Heal 2(3) HP. (Obtained from event: Vampires(?)).
+#Expunger : cost 1, Deal 9(15) damage X times. (Obtained from Conjure Blade).
+#insight : cost 0, Icon Retain Retain. Draw 2(3) cards. Exhaust. (Obtained from Evaluate, Pray and Study).
+#J.A.X. : cost 0, Lose 3 HP.Gain 2(3) Icon Strength Strength. (Obtained from event: Augmenter)
+#Miracle : cost 0, Icon Retain Retain. Gain (2) Energy. Exhaust. (Obtained from Collect, Deus Ex Machina, PureWater-0 Pure Water, and Holy water Holy Water).
+#Omega : cost 3 , At the end of your turn deal 50(60) damage to ALL enemies. (Obtained from Beta).
+#Ritual Dagger : cost 1, Deal 15 damage. If this kills an enemy then permanently increase this card's damage by 3(5) (Obtained during event: The Nest)
+#Safety : cost 1 , Icon Retain Retain. Gain 12(16) Icon Block Block. Exhaust. (Obtained from Deceive Reality).
+#Shiv : cost 0, Deal 4(6) damage. Exhaust. (Obtained from the Blade Dance, Cloak and Dagger, Infinite Blades, Storm of Steel, and NinjaScroll Ninja Scroll).
+#Smite : cost 1 , Icon Retain Retain. Deal 12(16) damage. Exhaust. (Obtained from Carve Reality and Battle Hymn).
+#Through Violence : cost 0, Icon Retain Retain. Deal 20(30) damage. Exhaust. (Obtained from Reach Heaven).
