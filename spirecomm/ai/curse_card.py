1f532e6f3d3dd6d9646b5065f1af9806b8081d26
SunJin Yoon <56009413+ysjin94@users.noreply.github.com>
2020-07-14 11:58:11 -0400

    Added functions

--- spirecomm/ai/curse_card.py
+++ spirecomm/ai/curse_card.py
@@ -1,1 +1,75 @@
 #This is curse_card
+
+import help_function
+
+#Ascender's Bane : Unplayable. Cannot be removed from your deck. Ethereal.
+def Ascenders_Bane(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+
+#Clumsy : Unplayable. ethereal
+def Clumsy(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+    
+#Curse of the Bell : Unplayable. Cannot be removed from your deck
+def Curse_of_the_Bell(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+    
+#Decay : Unplayable, At the end of your turn take 2 damagae
+def Decay(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+    
+#Doubt : Unplayable, At the end of your turn, gain 1 weak
+def Doubt(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+    
+#Injury : Unplayable
+def Injury(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+    
+#Necronomicurse : Unplayable, There is no escape form this Curse (Cannot be Exhausted or Removed from your deck)
+def Necronomicurse(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+
+#Normality : Unplayable, You cannot play more than 3 cards this turn
+def Normality(gamestat, upgradee):
+    newstate = gamestate
+    return newstate
+
+#Pain : Unplayable, while in had, lose 1 Hp when other cards are played.
+def Pain(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+
+#Parasite : Unplayable.If transformed or removed from your deck. lose 3 Max HP
+def Parasite(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+
+#Pride : Innate, Ate the end of turn, put a copy of this card on top of your draw pile. Exhuast
+#        Innate : Start each combat with this card in your hand
+def Pride(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+
+#Regret : Unplayable, At the end of your turn, lose HP equal to the number of cards in your hand.
+def Regret(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+
+#Shame : Unplayable. At the end of your turn, gain 1 Icon Frail Frail.
+def Shame(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+
+#Writhe : Unplayable. Innate
+def Writhe(gamestate, upgrade):
+    newstate = gamestate
+    return newstate
+    
